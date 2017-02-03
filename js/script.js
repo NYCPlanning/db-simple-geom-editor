467d9d0f071cdceae8d1151c01fa717cdbde0ba7
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-02-03 15:11:37 -0500

    changed basemap

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([40.706213526877455, -74.0044641494751], 15);
       
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
   maxZoom: 18,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
 }).addTo(map);
